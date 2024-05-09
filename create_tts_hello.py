d94edb9f1763a67e0af2182beab8749300015c52
zaruous <zaruous@naver.com>
2024-05-09 23:36:37 +0900

    tts vocies

--- create_tts_hello.py
+++ create_tts_hello.py
@@ -7,20 +7,22 @@ print(os.getenv("OPENAI_API_KEY"))
 # Please create a file named .env and place your
 # OpenAI key as OPENAI_API_KEY=xxx
 load_dotenv()
-os.get
+
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 output_dir = "demo_result"
 
-inputText = "안녕하세요 저는 김영준입니다."
+
 inputText = "This audio will be used to extract the base speaker tone color embedding. " + \
         "Typically a very short audio should be sufficient, but increasing the audio " + \
         "length will also improve the output audio quality."
+inputText = "안녕하세요 저는 김영준입니다."
 
+voices = ["alloy", "echo", "fable", "onyx", "nova", "shimmer"]
+for voice in voices:
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice=voice,
+        input=inputText,
 
-
-response = client.audio.speech.create(
-    model="tts-1",
-    voice="nova",
-    input=inputText
-)
-response.write_to_file(f"{output_dir}/openai_source_output.mp3")
+    )
+    response.write_to_file(f"{output_dir}/openai_source_{voice}_output.mp3")
