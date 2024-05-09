45cd91dae66ee967cc5d893bcc0658b65dcc8167
zaruous <zaruous@naver.com>
2024-05-10 00:20:00 +0900

    tts vocies

--- create_tts_hello.py
+++ create_tts_hello.py
@@ -15,7 +15,8 @@ output_dir = "demo_result"
 inputText = "This audio will be used to extract the base speaker tone color embedding. " + \
         "Typically a very short audio should be sufficient, but increasing the audio " + \
         "length will also improve the output audio quality."
-inputText = "안녕하세요 저는 김영준입니다."
+inputText = "안녕하세요 저는 김영준입니다. 만나서 반갑습니다. OpenVoice를 통한 테스트 음성입니다. " \
+            "현재 날짜는 2024년 5월 9일입니다. "
 
 voices = ["alloy", "echo", "fable", "onyx", "nova", "shimmer"]
 for voice in voices:
@@ -26,3 +27,4 @@ for voice in voices:
 
     )
     response.write_to_file(f"{output_dir}/openai_source_{voice}_output.mp3")
+
